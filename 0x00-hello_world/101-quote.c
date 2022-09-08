b2ee31165aaced4d1dd3f641c55c2835b755cfc8
zeboa <zerabyirga@gmail.com>
2022-09-08 14:37:03 -0700

    Its midnight man

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - print the size of variouse types
+ * Return: 1 if exited propely, non zero otherwise
+ *
+ */
+
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" 59);
+return (1);
+}
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,10 @@
 
 int main(void)
 {
-	printf("Size of a char: %z byte(s)\n", sizeof(char));
-	printf("Size of an int: %z byte(s)\n", sizeof(int));
-	printf("Size of a long int: %z byte(s)\n", sizeof(long int));
-	printf("Size of long long int: %z byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %z byte(s)\n", sizeof(float));
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of long long int: %dd byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
 		return (0);
 }
