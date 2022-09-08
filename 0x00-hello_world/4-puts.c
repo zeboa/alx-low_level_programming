e0a2b7de134049110ecf2e1739e96e312648cc6d
zeboa <zerabyirga@gmail.com>
2022-09-08 13:19:26 -0700

    C is going good

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints the required phrase
+ * Return: 0 if exited propely, non zero otherwise
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+		return (0);
+}
