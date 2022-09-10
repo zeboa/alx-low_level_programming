36fa39ce7e922e059bfa515465520ae0e0feecd0
zeboa <zerabyirga@gmail.com>
2022-09-10 06:32:33 -0700

    Commit

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints lowercase alphabet
+ * main - prints all single digit numbers of base 10 starting from 0
  * Return: 0 if exited propely, non zero otherwise
  */
 
@@ -9,7 +9,7 @@ int main(void)
 {
 	int n;
 	
-	for (n = 11; n = 21; n++)
+	for (n = 11; n < 21; n++)
 	{
 		putchar(n);
 	}
