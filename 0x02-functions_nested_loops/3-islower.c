0fb86284f7d4c049f398e3e51a9c553f3aa524f1
zeboa <zerabyirga@gmail.com>
2022-09-14 02:46:51 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower checks for lower characters
+ * @c: the character to check
+ * Return: 1 if characters are lower, 0 otherwise
+ */
+
+int _islower(int c);
+{
+	if (c >= 'a' && c <= 'z');
+	{
+		return (1);
+	}
+	else
+	{
+		return (0)
+	}
+	_putchar('\n')
+}
