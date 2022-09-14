43746beb24da94d3f9a46062d80d31900b802cee
zeboa <zerabyirga@gmail.com>
2022-09-14 01:32:53 -0700

    1 Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <main.h>
+
+/**
+ * main - prints lowercase alphabet
+ * Return: 0 if exited propely, non zero otherwise
+ */
+
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+	return (0);
+}
