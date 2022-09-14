1815d1690a7213337b4f6d38d3e1ad209820168d
zeboa <zerabyirga@gmail.com>
2022-09-14 12:47:41 -0700

    5 o 10

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,7 +15,7 @@ void times_table(void)
 			k = j * i;
 			if (j == 0)
 			{
-				putchar(k + '0');
+				_putchar(k + '0');
 			}
 
 			if (k < 10 && j != 0)
@@ -31,7 +31,7 @@ void times_table(void)
 				_putchar((k / 10) + '0');
 				_putchar((k % 10) + '0');
 			}
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
-
