5df89b9593054ba6351f0c5538810194d3b705a2
zeboa <zerabyirga@gmail.com>
2022-09-14 09:08:50 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - a function that prints the sign of a number
+ * @n: the int to check
+ * Return: 1 if characters letter, 0 otherwise
+ */
+
+int print_sign(int n)
+{
+	if (n < 0)
+	{	
+		_putchar('-');
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return ('0');
+	}
+	else if (n > 0)
+	{
+		_putchar('1');
+		return ('+');
+	}
+}
