4c0bcabefd9e33825cc8e65275bdc1bbe25d0dee
zeboa <zerabyirga@gmail.com>
2022-09-14 08:41:57 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,18 +1,13 @@
 #include "main.h"
 
 /**
- * _islower checks for lower characters
+ * _isalpha - checks for alphabetic character
  * @c: the character to check
- * Return: 1 if characters are lower, 0 otherwise
+ * Return: 1 if characters letter, 0 otherwise
  */
 
-int _islower(int c)
-{
-	if (c >= 'a' && c <= 'z')
-	{
-		return (1);
-	}
-	else
+int _isalpha(int c)
+{	
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 
-		return (0);
 }
