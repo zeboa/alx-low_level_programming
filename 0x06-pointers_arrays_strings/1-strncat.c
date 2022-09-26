4f4315c21121fd48e3867d2a53dd0db0ba722d18
zeboa <zerabyirga@gmail.com>
2022-09-26 13:46:12 +0300

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,18 +1,26 @@
 #include "main.h"
-#include <stdio.h>
 
-char *_strcat(char *dest, char *src)
+/**
+ * _strncat - concatenates n bytes from a string to another
+ * @dest: destination string
+ * @src: source sring
+ * @n: number of bytes of string to concatenate
+ * Return: a pointer to the resulting string dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
 {
 	int i = 0;
 	int j = 0;
 
-	while (dest[i] != "\0")
+	while (dest[i] != '\0')
 		i++;
 
-	while (src[j] != "\0")
+	while (src[j] != '\0' && j < n)
 	{
+	    dest[i] = src[j];
 		i++;
-		j++
+		j++;
 	}
 	dest[i] = '\0';
 
