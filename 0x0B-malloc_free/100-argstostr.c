a52d57f201e9858d6e79bbaf8797faf6ed05a2b7
zeboa <zerabyirga@gmail.com>
2022-10-04 06:34:16 +0300

    Commit first

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,45 +1,63 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "main.h"
 
 /**
- * *argstostr - concatenates all the arguments of the program
- * @ac: number of arguments
- * @av: array of arguments
- *
- * Return: Pointer to the new string (Success), NULL (Error)
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
  */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+		len++, s++;
+
+	return (len);
+}
+
+/**
+ * argstostr - concatenates all the arguments of your program
+ * @ac: argc
+ * @av: arguments
+ * Return: pointer to array
+ */
+
 char *argstostr(int ac, char **av)
 {
-	int i, j, k, len;
-	char *str;
+	char *s;
+	int len = 0, i, j, k = 0;
 
-	if (ac == 0 || av == NULL)
+	if (ac == 0 || av == NULL) /* validate input */
 		return (NULL);
 
+	/* find length to malloc */
 	for (i = 0; i < ac; i++)
 	{
-		for (j = 0; av[i][j] != '\0'; j++)
-			len++;
-		len++;
+		len += _strlen(av[i]);
+	}
+	len += (ac + 1); /* add space for newlines and null terminator */
+
+	/* allocate memory and free if error */
+	s = malloc(len * sizeof(char));
+
+	if (s == NULL)
+	{
+		free(s);
+		return (NULL);
 	}
 
-	str = malloc(sizeof(char) * (len + 1));
-
-	if (str == NULL)
-		return (NULL);
-
-	k = 0;
-
+	/* insert each arg into *str */
 	for (i = 0; i < ac; i++)
 	{
-		for (j = 0; av[i][j] != '\0'; j++)
+		for (j = 0; j < _strlen(av[i]); j++)
 		{
-			str[k] = av[i][j];
-			k++;
+			s[k++] = av[i][j];
 		}
-		str[k] = '\n';
-		k++;
+		s[k++] = '\n';
 	}
 
-	return (str);
+	return (s);
 }
