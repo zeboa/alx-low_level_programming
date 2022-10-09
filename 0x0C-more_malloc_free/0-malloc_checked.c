b0aaf02b5a19529ea4c0ea8ec20429d991122773
zeboa <zerabyirga@gmail.com>
2022-10-09 08:19:18 -0700

    ere betty

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -4,7 +4,7 @@
 /**
  * *malloc_checked - allocates memory using mallocs
  * @b: number of bytes to allocate
- * * Return: a pointer to the allocated memory
+ * Return: a pointer to the allocated memory
  */
 
 void *malloc_checked(unsigned int b)
