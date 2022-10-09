6854cfc3824bc74b420a29eefc69b8876b40ea75
zeboa <zerabyirga@gmail.com>
2022-10-09 08:07:10 -0700

    Commission

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,8 +2,8 @@
 #define MAIN_H
 
 int _putchar(char c);
-void *_calloc(unsigned int nmemb, unsigned int size);
 void *malloc_checked(unsigned int b);
+void *_calloc(unsigned int nmemb, unsigned int size);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
