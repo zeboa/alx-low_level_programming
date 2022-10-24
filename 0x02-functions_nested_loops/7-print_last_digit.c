b4f386ee324de9790fd20e79ecf5c94a02f3ae19
zeboa <zerabyirga@gmail.com>
2022-10-23 20:54:53 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,10 +8,17 @@
 
 int print_last_digit(int n)
 {
+	int a;
+
 	if (n < 0)
-		n *= -1;
+		n = -n;
 
-	_putchar('0' + (n % 10));
+	a = n % 10;
 
-	return (n % 10);
+	if (a < 0)
+		a = -a;
+
+	_putchar(a + '0');
+
+	return (a);
 }
